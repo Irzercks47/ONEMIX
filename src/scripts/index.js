f16bf07b96fd4f96c7f977576c23dafd7b89ebf9
MAditP <maditprinansyah@gmail.com>
2022-05-31 20:15:11 +0700

    initial commit

---
+++ src/scripts/index.js
@@ -0,0 +1,6 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+import './component/app-bar';
+import './component/jumbotron-bar';
+
+import '../styles/jumbotron.css';
