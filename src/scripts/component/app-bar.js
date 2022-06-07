8ed366edf192f56529b04d8a9790a8c1ab97c392
Irzercks47 <irzadiarifin@gmail.com>
2022-06-07 13:56:53 +0700

    adding bootstrap and popper to webpack and moving cardlist to src

--- src/scripts/component/app-bar.js
+++ src/scripts/component/app-bar.js
@@ -6,24 +6,24 @@ class appBar extends HTMLElement {
   render() {
     this.innerHTML = `
       <header>
-      <nav class="navbar navbar-expand-lg bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">ONEMIX</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-            <a class="nav-link" href="#">Features</a>
-            <a class="nav-link" href="#">Pricing</a>
-            <a class="nav-link disabled">Disabled</a>
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">ONEMIX</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+              <div class="navbar-nav">
+                <a class="nav-link" aria-current="page" href="#">Home</a>
+                <a class="nav-link" href="#">Features</a>
+                <a class="nav-link" href="#">Pricing</a>
+                <a class="nav-link disabled">Disabled</a>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </nav>
+        </nav>
       </header>
-          `;
+    `;
   }
 }
 
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,8 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import './component/app-bar';
-import './component/jumbotron-bar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+// import './component/jumbotron-bar';
 
-import '../styles/jumbotron.css';
+// import '../styles/jumbotron.css';
