2c0f0ab04fd62224a97bfab23b813bade870183c
Irzercks47 <irzadiarifin@gmail.com>
2022-06-08 15:54:47 +0700

    making routes for app bar and fixing home page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,16 @@
-import 'regenerator-runtime'; /* for async await transpile */
+import 'regenerator-runtime';
 import '../styles/main.css';
 import './component/app-bar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import './component/jumbotron-bar';
+import App from './views/app';
+
+const app = new App({
+    content: document.querySelector('main'),
+});
+
+window.addEventListener('hashchange', () => {
+    app.renderPage();
+});
 
--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -1,7 +1,6 @@
-const ListResto = {
+const Home = {
     async render() {
         return `
-            <jumbotron-bar></jumbotron-bar>
             <div class="js_list">
                 <h1 class="content-title">Catalogue</h1>
                 <div class="cataloguelist js_cataloguelist" id="cataloguelist"></div>
@@ -10,10 +9,8 @@ const ListResto = {
     },
 
     async afterRender() {
-        // const restaurants = await RestaurantDbSource.listRestaurant();
-        // const restaurantContainer = document.querySelector('#restaurantlist');
-        // restaurants.forEach((restaurant) => {
-        //     restaurantContainer.innerHTML += createRestaurantTemplate(restaurant);
-        // });
+        //smthng
     },
 };
+
+export default Home;
