982ccda95204140cc2ead6da70aa8600cabf84ac
Ahmad Hanif Mahfuddin <ahmadhanif320@gmail.com>
2022-06-09 18:33:07 +0700

    menambahkan aftercheckout-pagejs,create-creatorjs,menambahkan import aftercheckout-page pada indexjs, dan terakhir sampai export default di create-creatorjs,membuat aftercheckoutcss

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,12 @@
 import 'regenerator-runtime';
 import '../styles/main.css';
+import '../styles/aftercheckout.css'
 import './component/app-bar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import './component/jumbotron-bar';
 import App from './views/app';
-
+import './views/pages/aftercheckout-page';
 const app = new App({
     content: document.querySelector('main'),
 });
